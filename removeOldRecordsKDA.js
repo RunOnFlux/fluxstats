ccf8b75dc2409433c25a75030e666e92abfdafa6
Tadeas Kmenta <tadeas.kmenta@zel.cash>
2021-05-16 15:33:57 +0800

    removal of old kadena records

---
+++ removeOldRecordsKDA.js
@@ -0,0 +1,28 @@
+const config = require('config');
+const serviceHelper = require('./src/services/serviceHelper');
+
+async function removeRecords() {
+  let db = null;
+  const kadenaNodesCollection = config.database.kadena.collections.nodes;
+
+  db = await serviceHelper.connectMongoDb();
+
+  const database = db.db(config.database.kadena.database);
+
+  const daysInMiliseconds = 35 * 24 * 60 * 60 * 1000;
+  const currentTime = new Date().getTime();
+  const minimumTime = currentTime - daysInMiliseconds;
+
+  const query = {
+    roundTime: { $lt: minimumTime },
+  };
+  const projection = {};
+
+  const results = await serviceHelper.findInDatabase(database, kadenaNodesCollection, query, projection);
+  console.log(results[0]);
+
+  // const result = await serviceHelper.removeDocumentsFromCollection(database, kadenaNodesCollection, query);
+  // console.log(result);
+}
+
+removeRecords();
