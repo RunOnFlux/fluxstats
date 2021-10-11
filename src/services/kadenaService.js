bd11d53ce3c05ab2a8e946e3ef6d2d194b3b254f
Tadeas Kmenta <tadeas.kmenta@zel.cash>
2021-10-11 14:59:19 +0700

    kda v2.10.0

--- src/services/kadenaService.js
+++ src/services/kadenaService.js
@@ -185,8 +185,8 @@ async function beginKadena() {
   for (const index of stringOfTenChars) { // async inside
     const randomNumber = Math.floor((Math.random() * fluxnodelist.length));
     const kdaNodes = await getKadenaLocation(fluxnodelist[randomNumber]);
-    const kdaNodesValid = kdaNodes.filter((node) => (node.hash === 'localSpecificationsVersion11' || node.hash === 'localSpecificationsVersion12'));
-    const kdaNodesINValid = kdaNodes.filter((node) => (node.hash !== 'localSpecificationsVersion11' && node.hash !== 'localSpecificationsVersion12'));
+    const kdaNodesValid = kdaNodes.filter((node) => (node.hash === 'localSpecificationsVersion13' || node.hash === 'localSpecificationsVersion12'));
+    const kdaNodesINValid = kdaNodes.filter((node) => (node.hash !== 'localSpecificationsVersion13' && node.hash !== 'localSpecificationsVersion12'));
     kdaNodesValid.forEach((node) => {
       chainwebnodelocations.push(node.ip);
     });
