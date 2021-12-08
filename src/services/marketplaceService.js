1aa596609162736a2003bdbbe0d07b3da4d66c13
SyGem Limited <simon@sygem.com>
2021-12-08 11:21:39 +0000

    Correct github URL for marketplace apps list

--- src/services/marketplaceService.js
+++ src/services/marketplaceService.js
@@ -4,7 +4,7 @@ const serviceHelper = require('./serviceHelper');
 
 async function listApps(req, res) {
   try {
-    const appsList = await axios.get('https://raw.githubusercontent.com/RunOnFlux/fluxapi/marketplace/config/marketplaceApps.json');
+    const appsList = await axios.get('https://raw.githubusercontent.com/RunOnFlux/fluxapi/master/config/marketplaceApps.json');
     res.json({status: 'success', data: appsList.data});
   } catch (error) {
     const errMessage = serviceHelper.createErrorMessage(error.message, error.name, error.code);
