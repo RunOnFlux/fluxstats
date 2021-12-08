2acd1488cbdba2ca50f209850de3daf8eac336dc
SyGem Limited <simon@sygem.com>
2021-12-08 10:53:56 +0000

    Marketplace apps now served from static JSON on github

--- src/services/marketplaceService.js
+++ src/services/marketplaceService.js
@@ -1,24 +1,11 @@
-const config = require('config');
+const axios = require('axios');
 const log = require('../lib/log');
 const serviceHelper = require('./serviceHelper');
 
-let db = null;
-const databaseLink = config.database.marketplace.database;
-const appsCollection = config.database.marketplace.collections.apps;
-
 async function listApps(req, res) {
   try {
-    const database = db.db(databaseLink);
-    log.info('list apps');
-    const query = {};
-    const projection = {
-      projection: {
-        _id: 0, // all except id
-      },
-    };
-    const results = await serviceHelper.findInDatabase(database, appsCollection, query, projection);
-    const resMessage = serviceHelper.createDataMessage(results);
-    res.json(resMessage);
+    const appsList = await axios.get('https://raw.githubusercontent.com/RunOnFlux/fluxapi/marketplace/config/marketplaceApps.json');
+    res.json({status: 'success', data: appsList.data});
   } catch (error) {
     const errMessage = serviceHelper.createErrorMessage(error.message, error.name, error.code);
     res.json(errMessage);
@@ -26,24 +13,6 @@ async function listApps(req, res) {
   }
 }
 
-async function start() {
-  try {
-    db = await serviceHelper.connectMongoDb().catch((error) => {
-      log.error(error);
-      throw error;
-    });
-    const database = db.db(config.database.local.database);
-    database.collection(appsCollection).createIndex({ hash: 1 }, { name: 'query for getting specific app' });
-    log.info('Initiating Marketplace services...');
-  } catch (e) {
-    // restart service after 5 mins
-    log.error(e);
-    setTimeout(() => {
-      start();
-    }, 5 * 30 * 1000);
-  }
-}
 module.exports = {
-  start,
   listApps,
 };
