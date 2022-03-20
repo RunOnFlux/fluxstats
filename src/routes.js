10fffc798e8a9df7a0b85b749d565367682eb2a3
Tadeas Kmenta <kmentat@gmail.com>
2022-03-20 20:52:01 +0700

    adjust kda processing for global app

--- src/routes.js
+++ src/routes.js
@@ -37,45 +37,8 @@ module.exports = (app) => {
     fluxService.fluxNodesHistoryStats(req, res);
   });
 
-  app.get('/kadena/outdatednodes', cache('5 minutes'), (req, res) => {
-    kadenaService.outdatedNodes(req, res);
-  });
-  app.get('/kadena/uptodatenodes', cache('5 minutes'), (req, res) => {
-    kadenaService.uptodateNodes(req, res);
-  });
-  app.get('/kadena/allnodes', cache('5 minutes'), (req, res) => {
-    kadenaService.allNodesAPI(req, res);
-  });
-  app.get('/kadena/availabletimes', cache('5 minutes'), (req, res) => {
-    kadenaService.getCompletedRoundsTimestamps(req, res);
-  });
-  app.get('/kadena/history/ip/:ip?', cache('5 minutes'), (req, res) => {
-    kadenaService.getKadenaIPHistory(req, res);
-  });
-  app.get('/kadena/history/account/:account?', cache('5 minutes'), (req, res) => {
-    kadenaService.getKadenaAccountHistory(req, res);
-  });
-  app.get('/kadena/limitedhistory/ip/:ip?/:days?', cache('5 minutes'), (req, res) => {
-    kadenaService.getKadenaIPHistoryDays(req, res);
-  });
-  app.get('/kadena/limitedhistory/account/:account?/:days?', cache('5 minutes'), (req, res) => {
-    kadenaService.getKadenaAccountHistoryDays(req, res);
-  });
-  app.get('/kadena/nodes/:timestamp?', cache('5 minutes'), (req, res) => {
-    kadenaService.getKadenaNodesForTimestamp(req, res);
-  });
-  app.get('/kadena/eligible/:days?', cache('10 minutes'), (req, res) => {
-    kadenaService.getKadenaEligibleDays(req, res);
-  });
-  app.get('/kadena/eligiblestats/:days?', cache('30 minutes'), (req, res) => {
-    kadenaService.getKadenaEligibleStatsDays(req, res);
-  });
-  app.get('/kadena/availabletimes', cache('5 minutes'), (req, res) => {
-    kadenaService.getCompletedRoundsTimestamps(req, res);
-  });
-
-  app.get('/kadena', cache('5 minutes'), (req, res) => {
-    res.sendFile(path.join(__dirname, './kdaUI/index.html'));
+  app.get('/kadena/nodes', cache('1 minute'), (req, res) => {
+    kadenaService.getKadenNodes(req, res);
   });
 
   app.get('/hashes', cache('1 minute'), (req, res) => {
