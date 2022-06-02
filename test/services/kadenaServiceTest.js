0c48af6810cd787bc9f42d116b1e1ca26ad45574
ra.andasan <ra.andasan@gmail.com>
2022-06-02 20:57:49 +0800

    Additional test cases for kadena service

--- test/services/kadenaServiceTest.js
+++ test/services/kadenaServiceTest.js
@@ -6,18 +6,35 @@ chai.use(require('chai-json-schema'));
 
 module.exports = () => {
     describe('Kadena Service Test', () => {
-        it('42. should return kadena nodes', async () => {
+        it('41. should run process kadena app locations node and return data', async () => {
+            const response = await service.kadenaAppLocationsNode();
+            chai.expect(response.data).to.not.be.null;
+            chai.expect(response.status).to.equal('success');
+        });
+
+        it('42. should run process kadena app location and return data', async () => {
+            const response = await service.kadenaAppLocations();
+            chai.expect(response.data).to.not.be.null;
+            chai.expect(response.status).to.equal('success');
+        });
+
+        it('43. should run process nodes and return data', async () => {
+            const response = await service.getNodes();
+            chai.expect(response.data).to.not.be.null;
+            chai.expect(response.status).to.equal('success');
+        });
+
+        it('44. should return kadena nodes', async () => {
             const req = {};
             const res = {
                 json: (data) => JSON.stringify(data),
             };
             const response = await service.getKadenaNodes(req, res);
-            console.log(response);
-            chai.expect(response).to.not.be.null;
+            chai.expect(response.data).to.not.be.null;
             chai.expect(response.status).to.equal('success');
         });
 
-        it('43. should run process KDA', async () => {
+        it('45. should run process KDA', async () => {
             chai.expect(service.processKDA(req, res));
         });
     });   
