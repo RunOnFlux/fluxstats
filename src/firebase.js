47eb6a3da8b7e393f2e66935828ea58d43fea88c
Jeff Vader <simon@sygem.com>
2023-03-20 12:38:06 +0000

    Check config before initialising Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,16 @@
-const firebase = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
-firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount)
-});
+const config = require('config');
+const log = require('./lib/log');
+
+let firebase;
+
+if (config.firebase) {
+  firebase = require("firebase-admin");
+  const serviceAccount = require("./serviceAccountKey.json");
+  firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount)
+  });
+  log.debug('Firebase initialized');
+}
 
 module.exports = {
   firebase,
