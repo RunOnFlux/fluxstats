a673eb168e7b9fa39b1269e214ba1e224ef676ed
TheTrunk <tadeas.kmenta@zel.cash>
2024-03-16 14:46:35 +0700

    add compression

--- src/lib/server.js
+++ src/lib/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const cors = require('cors');
+const compression = require('compression');
 
 const nodeEnv = process.env.NODE_ENV;
 
@@ -11,6 +12,7 @@ if (nodeEnv !== 'test') {
   app.use(morgan('combined'));
 }
 
+app.use(compression());
 app.use(cors({ origin: '*' }));
 const fluxStatsUI = path.join(__dirname, '../../ui/dist');
 app.use(express.static(fluxStatsUI));
